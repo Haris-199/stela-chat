b21096a40b8eff596115e19a86c47f38552f5236
Haris Siddiqui <haris503@hotmail.com>
2025-06-08 07:50:41 -0400

    Create prisma client

---
+++ server/src/db/client.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "./generated/prisma";
+
+const databaseUrl =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+export const prisma = new PrismaClient({
+  datasources: {
+    db: { url: databaseUrl },
+  },
+});
