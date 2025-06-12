343f83926033d61c47f2f320788bae0836984a3b
Haris Siddiqui <haris503@hotmail.com>
2025-06-11 22:52:30 -0400

    Fix cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,13 +6,18 @@ import router from "./routes";
 import "dotenv/config";
 
 const PORT = Number(process.env.PORT) || 3000;
-const CLIENT_URL = process.env.CLIENT_URL || "http://127.0.0.1:5173";
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 
 const app = express();
 
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(cors({ origin: CLIENT_URL }));
+app.use(cors({
+  origin: CLIENT_URL,
+  optionsSuccessStatus: 200,
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", router);
