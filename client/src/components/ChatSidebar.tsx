98e4d3053082223c47a3728d42c9af83c83ce3de
Haris Siddiqui <haris503@hotmail.com>
2025-07-12 08:08:45 -0400

    Refactor chat sidebar skeleton

--- client/src/components/ChatSidebar.tsx
+++ client/src/components/ChatSidebar.tsx
@@ -1,7 +1,6 @@
 import Avatar from "./Avatar";
 import { getChats } from "../services/api";
 import { Chat, UserPayload } from "../types";
-import { Plus } from "lucide-react";
 import { Link, Navigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
@@ -18,7 +17,7 @@ export default function ChatSidebar({
 }) {
   const {
     data: chats,
-    isLoading,
+    isPending,
     error,
   } = useQuery({
     queryFn: () => getChats(userData).then((res) => res.data),
@@ -30,10 +29,6 @@ export default function ChatSidebar({
       setCurrentChat(chats.find((chat) => chat.id === chatId) || null);
     }
   }, [chatId, chats, setCurrentChat]);
-
-  if (isLoading) {
-    return <SkeletonLoader />;
-  }
 
   if (error) {
     return <Navigate to="/500" />;
@@ -49,7 +44,9 @@ export default function ChatSidebar({
         className="bg-radial-[at_0%_-50%] from-primary-900 to-primary-700 flex-1 flex flex-col gap-3 px-2 py-2 overflow-y-auto"
         style={{ scrollbarColor: "var(--color-primary-700) var(--color-primary-600)" }}
       >
-        {chats &&
+        {isPending && chats === undefined ? (
+          <SkeletonLoader />
+        ) : (
           chats.map((chat) => (
             <Link
               to={`/chat/${chat.id}`}
@@ -65,7 +62,8 @@ export default function ChatSidebar({
               <Avatar letter={chat.name[0]} className="size-10" />
               <span className="truncate">{chat.name}</span>
             </Link>
-          ))}
+          ))
+        )}
       </nav>
     </aside>
   );
@@ -73,28 +71,16 @@ export default function ChatSidebar({
 
 function SkeletonLoader() {
   return (
-    <aside className="w-64 text-white bg-radial-[at_0%_-50%] from-primary-900 to-primary-700 flex flex-col shadow-lg">
-      <h2 className="px-6 py-1 flex justify-between items-center text-primary-100 bg-gradient-to-r from-primary-800 to-primary-700 shadow-md">
-        <p className="font-bold text-lg">Chats</p>
-        <button
-          className="flex flex-col items-center p-2 rounded-lg hover:bg-primary-700"
-          title="New Chat"
+    <>
+      {[...Array(5)].map((_, i) => (
+        <div
+          key={i}
+          className="animate-pulse first-of-type:mt-1 flex items-center gap-3 px-3 py-2 rounded-lg bg-primary-700/60 shadow-sm"
         >
-          <Plus size={20} />
-          <span className="text-xs mt-1">New</span>
-        </button>
-      </h2>
-      <nav className="flex-1 flex flex-col gap-3 px-2 overflow-y-auto bg-radial-[at_0%_-50%] from-primary-900 to-primary-700 py-2">
-        {[...Array(5)].map((_, i) => (
-          <div
-            key={i}
-            className="animate-pulse first-of-type:mt-1 flex items-center gap-3 px-3 py-2 rounded-lg bg-primary-700/60 shadow-sm"
-          >
-            <div className="bg-primary-500/40 rounded-full size-10" />
-            <div className="h-4 bg-primary-500/40 rounded w-2/3" />
-          </div>
-        ))}
-      </nav>
-    </aside>
+          <div className="bg-primary-500/40 rounded-full size-10" />
+          <div className="h-4 bg-primary-500/40 rounded w-[75%]" />
+        </div>
+      ))}
+    </>
   );
 }
