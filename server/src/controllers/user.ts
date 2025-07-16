a904c2c4bca931cb83184d7491983a9eae4133e5
Haris Siddiqui <haris503@hotmail.com>
2025-07-16 08:08:42 -0400

    Add routes for friend requests

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -29,3 +29,156 @@ export async function getFriend(req: Request, res: Response, next: NextFunction)
     next(error);
   }
 }
+
+export async function getFriendRequest(req: Request, res: Response, next: NextFunction) {
+  try {
+    const requests = await prisma.friendRequest.findMany({
+      where: { receiverId: req.user!.id },
+      select: { id: true, sender: { select: { username: true } } },
+    });
+    res.status(200).json({
+      success: true,
+      message: "Incoming friend requests retrieved successfully.",
+      data: requests.map((request) => ({ id: request.id, username: request.sender.username })),
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function postFriendRequest(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { receiver } = req.body;
+
+    if (receiver === req.user!.username) {
+      res.status(400).json({ success: false, message: "Can not send friend request to yourself." });
+      return;
+    }
+
+    const recipient = await prisma.user.findUnique({
+      where: { username: receiver },
+      include: { friends: true },
+    });
+
+    if (recipient === null) {
+      res.status(404).json({ success: false, message: "Receiver not found." });
+      return;
+    }
+
+    if (recipient.friends.some((friend) => friend.username === req.user!.username)) {
+      res.status(400).json({
+        success: false,
+        message: `User is already friends with "${receiver}".`,
+      });
+      return;
+    }
+
+    const existing = await prisma.friendRequest.findFirst({
+      where: { senderId: req.user!.id, receiver: { username: receiver } },
+    });
+
+    if (existing !== null) {
+      res.status(400).json({ success: false, message: "Request already exists." });
+      return;
+    }
+
+    await prisma.friendRequest.create({
+      data: {
+        sender: { connect: { id: req.user!.id } },
+        receiver: { connect: { username: receiver } },
+      },
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Friend request sent successfully.",
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function putFriendRequest(req: Request, res: Response, next: NextFunction) {
+  try {
+    const receiver = req.user!;
+    const id = +req.params.id; // Id of the friend request to respond to
+    const { sender } = req.body;
+
+    const senderUser = await prisma.user.findUnique({
+      where: { username: sender },
+      include: { sentFriendRequests: { where: { id }, take: 1 } },
+    });
+
+    if (senderUser === null) {
+      res.status(404).json({
+        success: false,
+        message: "Sender not found.",
+      });
+      return;
+    }
+
+    if (senderUser.sentFriendRequests.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "Friend request not found.",
+      });
+      return;
+    }
+
+    if (senderUser.sentFriendRequests[0].receiverId !== receiver.id) {
+      res.status(422).json({
+        success: false,
+        message: "Friend request is not valid.",
+      });
+      return;
+    }
+
+    const otherRequest = await prisma.friendRequest.findFirst({
+      where: { AND: [{ receiverId: senderUser.id }, { senderId: receiver.id }] },
+    });
+
+    await Promise.all([
+      prisma.friendRequest.delete({ where: { id } }),
+      prisma.user.update({
+        where: { username: receiver.username },
+        data: {
+          friends: { connect: { username: sender } },
+          friendOf: { connect: { username: sender } },
+        },
+      }),
+      otherRequest !== null && prisma.friendRequest.delete({ where: { id: otherRequest.id } }),
+    ]);
+
+    res.status(200).json({
+      success: true,
+      message: "Friend request responded successfully.",
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function deleteFriendRequest(req: Request, res: Response, next: NextFunction) {
+  try {
+    const id = +req.params.id;
+    const user = req.user!;
+
+    const request = await prisma.friendRequest.findUnique({ where: { id } });
+
+    if (request === null || request.senderId !== user.id) {
+      res.status(404).json({
+        success: false,
+        message: "Friend request not found.",
+      });
+      return;
+    }
+
+    await prisma.friendRequest.delete({ where: { id } });
+    res.status(200).json({
+      success: true,
+      message: "Friend request canceled successfully.",
+    });
+  } catch (error) {
+    next(error);
+  }
+}
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
-import { getUser, getFriend } from "../controllers/user";
+import {
+  getUser,
+  getFriend,
+  getFriendRequest,
+  postFriendRequest,
+  putFriendRequest,
+  deleteFriendRequest,
+} from "../controllers/user";
 import { authenticateJWT } from "../middleware/auth";
 
 const router = Router();
 
 router.get("/", getUser);
-router.get("/friend", authenticateJWT, getFriend);
+router.use("/friend", authenticateJWT);
+router.get("/friend", getFriend);
+router.get("/friend/request", getFriendRequest);
+router.post("/friend/request", postFriendRequest);
+router.put("/friend/request/:id", putFriendRequest);
+router.delete("/friend/request/:id", deleteFriendRequest);
 
 export default router;
