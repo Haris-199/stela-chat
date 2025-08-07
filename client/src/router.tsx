c671852f982cfb33884e551febaa2a6bb2e0b37f
Haris Siddiqui <haris503@hotmail.com>
2025-08-07 19:37:06 -0400

    Remove profile

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Chat from "./pages/Chat";
-import Profile from "./pages/Profile";
 import ServerError from "./pages/ServerError";
 import NotFound from "./pages/NotFound";
 
@@ -13,7 +12,6 @@ const router = createBrowserRouter([
   { path: "register", element: <Register /> },
   { path: "chat/:chatId", element: <Chat /> },
   { path: "chat", element: <Chat /> },
-  { path: "profile", element: <Profile /> },
   { path: "500", element: <ServerError /> },
   { path: "*", element: <NotFound /> },
 ]);
