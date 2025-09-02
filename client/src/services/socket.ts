9ae3025894e1572768675a5739222edddf3ae731
Haris Siddiqui <haris503@hotmail.com>
2025-09-02 06:10:18 -0400

    Add proxy for web socket

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export default function createSocket(token: string) {
-  return io(import.meta.env.VITE_SERVER_URL, {
+  return io(undefined, {
     autoConnect: false,
     auth: { token },
   });
